bba26b464f10654b561612150cbe6530b504e1a1
Cyril Kato <cyril@users.noreply.github.com>
2016-11-13 23:36:36 +0100

    add a PHP script file to check authentication

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+
+good_username = "bob";
+good_password = "super-secret";
+
+// Remplacer False par une expression booléenne pour tester les paramètres reçus
+if (False) {
+  echo "Vous etes connecté !";
+} else {
+  echo "Mauvais nom d'utilisateur ou mot de passe."
+}
+
+?>
